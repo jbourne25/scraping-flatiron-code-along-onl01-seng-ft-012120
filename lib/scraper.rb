324aeaba22f63e5cd2646a7f5f66c0b80cf1ca48
Jeremy Bourne <jbourne25@yahoo.com>
2020-02-08 06:36:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,6 +23,12 @@ class Scraper
     def get_courses 
       self.get_page.css(".post")
     end 
+    
+    def make_courses
+      self.get_courses.each do |post|
+        course = Course.new 
+        
+    end 
   
   
   
